636ebf8f0ff4534e16e5db1a590ed959526d6f34
shicongguncode <145893667+shicongguncode@users.noreply.github.com>
2023-09-25 14:42:18 +0800

    Update HelloGit.java

--- src/main/java/com/apply/HelloGit.java
+++ src/main/java/com/apply/HelloGit.java
@@ -8,7 +8,7 @@ public class HelloGit {
         System.out.println("第四次提交");
         System.out.println("dev分支");
         System.out.println("本地master ====》远程dev");
-        System.out.println("本地dev ===》远程master111111");
+        System.out.println("本地dev ===》远程master666666");
 
     }
 }
