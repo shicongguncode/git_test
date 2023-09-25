3a9e5170a8a9d82f8c0c94365abf611e75b6de22
shiconggun <shiconggun@gmail.com>
2023-09-25 14:07:10 +0800

    dev分支

--- src/main/java/com/apply/HelloGit.java
+++ src/main/java/com/apply/HelloGit.java
@@ -4,6 +4,9 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("第一次提交");
         System.out.println("第二次提交");
+        System.out.println("第三次提交");
+        System.out.println("第四次提交");
+        System.out.println("dev分支");
 
     }
 }
